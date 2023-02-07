a1b5e9c43ba1c6c5be79520e493295c68204e28c
송병훈 <thdqudgns9612@gmail.com>
2023-02-08 01:48:51 +0900

    Update 스위치 켜고 끄기.java

--- 송병훈/BOJ/Silver - 1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 송병훈/BOJ/Silver - 1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N, S;		// 스위치 수, 학생 수 -> 고정
 	static int[] switchs;	// 스위치 상태 -> 변함
-	static int sex, num;	// 성별, 번호 -> 입력 받을 때마다 덮어씌울 예정
+	static int gender, num;	// 성별, 번호 -> 입력 받을 때마다 덮어씌울 예정
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -22,11 +22,11 @@ public class Main {
 		
 		for (int i = 0; i < S; i++) {
 			st = new StringTokenizer(br.readLine());
-			sex = Integer.parseInt(st.nextToken()); // 성별
+			gender = Integer.parseInt(st.nextToken()); // 성별
 			num = Integer.parseInt(st.nextToken());	// 번호
 			
 			// switchs는 static이라 안 넘겨도 된다. 성별은 메소드를 특정했으니 상관없다.
-			if (sex == 1) manChange(num);	
+			if (gender == 1) manChange(num);	
 			else womanChange(num-1, 0);		// 스위치의 번호는 0이 아니라, 1부터 시작하므로 index를 하나 감소시켜 넘긴다.
 		}
 		
